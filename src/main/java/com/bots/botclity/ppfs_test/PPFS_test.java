2190b2e113ee89a0fefa2a801d42f149651927f0
BotClity <kirilllbcnk@gmail.com>
2022-01-09 18:16:03 +0300

    Update PPFS_test.java

--- src/main/java/com/bots/botclity/ppfs_test/PPFS_test.java
+++ src/main/java/com/bots/botclity/ppfs_test/PPFS_test.java
@@ -13,29 +13,33 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.io.File;
 
 public final class PPFS_test extends JavaPlugin {
-    //Register bot and get chat id
+    //Register telegram bot (GitHub) and get chat id
     TelegramBot bot = new TelegramBot(this.getConfig().getString("Bot.Token"));
     String chatId = this.getConfig().getString("Bot.Chat_Id");
     private final JavaPlugin plugin = this;
 
     @Override
     public void onEnable() {
+        //Check if there is a config.yml
         File config = new File(getDataFolder() + File.separator + "config.yml");
         if (!config.exists()){
+            //Create config
             getLogger().info("Creating config...");
             getConfig().options().copyDefaults(true);
             saveDefaultConfig();
             this.getLogger().warning("Please configure config.yml");
+            //Turn off plugin
             Bukkit.getPluginManager().disablePlugin(this);
         }
 
-
+        //Send messages to telegram and server
         bot.execute(new SendMessage(chatId, "Server enabled"));
-        Bukkit.getPluginManager().registerEvents(new sendMessage(plugin),this);
         Bukkit.getLogger().info("Plugin enabled.");
+        //Register event listener
+        Bukkit.getPluginManager().registerEvents(new sendMessage(plugin),this);
 
 
-
+        //Register telegram bot(Api)
         bot bot = new bot(plugin);
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
@@ -48,8 +52,9 @@ public final class PPFS_test extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        ////Send messages to telegram and server
         bot.execute(new SendMessage(chatId, "Server disabled"));
         Bukkit.getLogger().info("Plugin disabled");
-        // Plugin shutdown logic
+
     }
 }
